26c37c70bd23f33b02e1dbeb3a1ca3b4a5b07211
katsuyorii <katsuyoriiii@gmail.com>
2025-06-24 19:16:58 +0300

    Create products app and router

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 from auth.routers import auth_router
 from users.routers import users_router
+from products.routers import products_router
 
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(users_router)
+app.include_router(products_router)
